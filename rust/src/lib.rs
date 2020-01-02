29df3db6a0f48b841e1304223173b7e6cc53480b
Oleg Utkin <oleg_utkin@me.com>
2020-01-02 19:07:54 +0300

    refactor

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,42 +9,70 @@ mod tarantool;
 
 use std::os::raw::{c_int, c_char};
 use std::slice;
-
-use tarantool::{BoxFunctionCtx};
-use crate::tarantool::{box_tuple_format_default, box_tuple_new, box_return_tuple};
+use tarantool::{
+    BoxFunctionCtx,
+    box_tuple_format_default,
+    box_tuple_new,
+    box_return_tuple,
+    box_space_id_by_name,
+    BOX_ID_NIL,
+    box_replace,
+    BoxTuple,
+};
+use serde::Serialize;
 
 #[derive(Debug, Deserialize, PartialEq)]
 struct Args<'a> {
+    uuid: &'a str,
     some_str: &'a str,
     another_str: &'a str,
 }
 
 #[no_mangle]
-pub extern fn rustproc(ctx: *mut BoxFunctionCtx, args_begin: *const u8, args_end: *const u8) -> c_int {
+pub extern fn rustproc(ctx: *mut BoxFunctionCtx, args_begin: *const c_char, args_end: *const c_char) -> c_int {
     assert!(!ctx.is_null());
     assert!(!args_begin.is_null());
     assert!(!args_end.is_null());
 
-    let args_blob = unsafe {
+    let reader = unsafe {
         let args_len = args_end as usize - args_begin as usize;
-        slice::from_raw_parts(args_begin, args_len)
+        &mut slice::from_raw_parts(args_begin as *const u8, args_len)
     };
 
-    let reader = &mut args_blob.as_ref();
     let args: Args = rmp_serde::from_read_ref(reader).unwrap();
-
     println!("{:?}", args);
 
-    let buffer = rmp_serde::to_vec(&(args.some_str, args.another_str)).unwrap();
+    const SPACE_NAME: &str = "space";
+    let space_id = unsafe {
+        box_space_id_by_name(SPACE_NAME.as_bytes().as_ptr() as *const c_char,
+                             SPACE_NAME.len() as u32)
+    };
+    assert_ne!(space_id, BOX_ID_NIL);
+
+    println!("space id: {}", space_id);
+
+    unsafe {
+        let res = box_replace(space_id, args_begin, args_end, 0 as *mut *mut BoxTuple);
+        assert_eq!(res, 0);
+    };
+
+    // serialize response to msgpack and return from procedure
+    respond(ctx, &(args.some_str, args.another_str));
+
+    return 0;
+}
+
+fn respond<T>(ctx: *mut BoxFunctionCtx, payload: &T) where T: Serialize + ?Sized {
+    let buffer = rmp_serde::to_vec(payload).unwrap();
     let tuple = unsafe {
         let fmt = box_tuple_format_default();
         let end = buffer.as_ptr().offset(buffer.len() as isize);
         box_tuple_new(fmt, buffer.as_ptr() as *const i8, end as *const i8)
     };
+    assert!(!tuple.is_null());
 
     unsafe {
-        box_return_tuple(ctx, tuple);
+        let res = box_return_tuple(ctx, tuple);
+        assert_eq!(res, 0);
     };
-
-    return 0;
 }
