9535ed4068be2b80273a0a25d45ce6e5b6aceb34
Oleg Utkin <o.utkin@corp.mail.ru>
2020-01-14 18:56:45 +0300

    wip

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,21 +7,21 @@ extern crate rmp_serde as rmps;
 #[allow(dead_code)]
 mod tarantool;
 
+pub mod tnt_box;
+
+use tnt_box::{
+    BoxCtx,
+    proc_error
+};
+
 use std::os::raw::{c_int, c_char};
-use std::slice;
 use tarantool::{
     BoxFunctionCtx,
-    box_tuple_format_default,
-    box_tuple_new,
-    box_return_tuple,
-    box_space_id_by_name,
-    BOX_ID_NIL,
-    box_replace,
-    BoxTuple,
 };
-use serde::Serialize;
+use std::error::Error;
+use crate::tnt_box::replace;
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 struct Args<'a> {
     uuid: &'a str,
     some_str: &'a str,
@@ -29,50 +29,25 @@ struct Args<'a> {
 }
 
 #[no_mangle]
-pub extern fn rustproc(ctx: *mut BoxFunctionCtx, args_begin: *const c_char, args_end: *const c_char) -> c_int {
-    assert!(!ctx.is_null());
-    assert!(!args_begin.is_null());
-    assert!(!args_end.is_null());
+pub extern fn rustproc(_ctx: *mut BoxFunctionCtx, _args_begin: *const c_char, _args_end: *const c_char) -> c_int {
+    let ctx = BoxCtx::new(_ctx, _args_begin, _args_end);
 
-    let reader = unsafe {
-        let args_len = args_end as usize - args_begin as usize;
-        &mut slice::from_raw_parts(args_begin as *const u8, args_len)
-    };
-
+    let reader = ctx.get_read_ref();
     let args: Args = rmp_serde::from_read_ref(reader).unwrap();
-    println!("{:?}", args);
-
-    const SPACE_NAME: &str = "space";
-    let space_id = unsafe {
-        box_space_id_by_name(SPACE_NAME.as_bytes().as_ptr() as *const c_char,
-                             SPACE_NAME.len() as u32)
-    };
-    assert_ne!(space_id, BOX_ID_NIL);
-
-    println!("space id: {}", space_id);
-
-    unsafe {
-        let res = box_replace(space_id, args_begin, args_end, 0 as *mut *mut BoxTuple);
-        assert_eq!(res, 0);
-    };
-
-    // serialize response to msgpack and return from procedure
-    respond(ctx, &(args.some_str, args.another_str));
+    match rust_procedure(&ctx, &args) {
+        Ok(()) => (),
+        Err(err) => proc_error(file!(), line!(), err.description()),
+    }
 
     return 0;
 }
 
-fn respond<T>(ctx: *mut BoxFunctionCtx, payload: &T) where T: Serialize + ?Sized {
-    let buffer = rmp_serde::to_vec(payload).unwrap();
-    let tuple = unsafe {
-        let fmt = box_tuple_format_default();
-        let end = buffer.as_ptr().offset(buffer.len() as isize);
-        box_tuple_new(fmt, buffer.as_ptr() as *const i8, end as *const i8)
-    };
-    assert!(!tuple.is_null());
+fn rust_procedure(ctx: &BoxCtx, args: &Args) -> Result<(), Box<dyn Error>> {
+    println!("{:?}", args);
 
-    unsafe {
-        let res = box_return_tuple(ctx, tuple);
-        assert_eq!(res, 0);
-    };
+    const SPACE_NAME: &'static str = "space";
+    replace(SPACE_NAME, &args)?;
+    ctx.respond("some string")?;
+
+    Ok(())
 }
---
+++ rust/src/tnt_box.rs
@@ -0,0 +1,149 @@
+use crate::tarantool::{BoxFunctionCtx, box_tuple_format_default, box_tuple_new, box_return_tuple, box_space_id_by_name, BOX_ID_NIL, box_replace, BoxTuple, box_error_set, box_tuple_ref, box_tuple_unref, box_error_last, BoxErrorype, box_error_message};
+use serde::Serialize;
+use std::os::raw::c_char;
+use std::{slice, fmt};
+use std::error::Error;
+use std::ffi::CStr;
+use std::convert::TryFrom;
+//use syn::proc_macro::TokenStream;
+//use syn::{parse_macro_input, parse_quote, Expr, Ident, ItemFn, Local, Pat, Stmt, Token};
+
+
+//type Tuple = BoxTuple;
+//impl Tuple {
+//
+//}
+
+//#[proc_macro_attribute]
+//pub fn tarantool_procedure(args: TokenStream, input: TokenStream) -> TokenStream {
+//    let input = parse_macro_input!(input as ItemFn);
+////    let x = format!(r#"
+////            fn dummy() {{
+////                println!("entering");
+////                println!("args tokens: {{}}", {args});
+////                println!("input tokens: {{}}", {input});
+////                println!("exiting");
+////            }}
+////        "#,
+////        args = args.into_iter().count(),
+////        input = input.into_iter().count(),
+////    );
+////
+////    x.parse().expect("Generated invalid tokens")
+//}
+
+pub struct BoxCtx {
+    _ctx: *mut BoxFunctionCtx,
+    _args_begin: *const c_char,
+    _args_end: *const c_char,
+}
+
+#[derive(Debug, Clone)]
+pub struct BoxError {
+    error_type: &'static str,
+    error_message: &'static str,
+}
+impl fmt::Display for BoxError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.error_type, self.error_message)
+    }
+}
+impl Error for BoxError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+fn get_last_error() -> BoxError {
+    let box_error = unsafe {
+        box_error_last()
+    };
+
+    let error_type = unsafe {
+        let c_str = BoxErrorype(box_error);
+        CStr::from_ptr(c_str).to_str().unwrap()
+    };
+
+    let error_message = unsafe {
+        let c_str = box_error_message(box_error);
+        CStr::from_ptr(c_str).to_str().unwrap()
+    };
+
+    BoxError{error_type, error_message}
+}
+
+impl BoxCtx {
+    pub fn new(_ctx: *mut BoxFunctionCtx, _args_begin: *const c_char, _args_end: *const c_char) -> BoxCtx {
+        BoxCtx { _ctx, _args_begin, _args_end }
+    }
+
+    pub fn respond<T>(&self, payload: &T) -> Result<(), Box<dyn Error>>
+        where T: Serialize + ?Sized
+    {
+//        let mut buffer: [u8; 4096] = [0; 4096];
+//        let writer = &mut buffer.as_mut();
+//        rmp_serde::encode::write(writer, payload).unwrap();
+        let buffer = rmp_serde::to_vec(payload)?;
+
+        let tuple = unsafe {
+            let fmt = box_tuple_format_default();
+            let end = buffer.as_ptr().offset(buffer.len() as isize);
+            box_tuple_new(fmt, buffer.as_ptr() as *const i8, end as *const i8)
+        };
+        assert!(!tuple.is_null());
+
+        unsafe {
+            box_tuple_ref(tuple);
+            let res = box_return_tuple(self._ctx, tuple);
+            box_tuple_unref(tuple);
+
+            if res != 0 {
+                return Err(Box::try_from(get_last_error()).unwrap())
+            }
+
+            assert_eq!(res, 0);
+        };
+
+        Ok(())
+    }
+
+    pub fn get_read_ref(&self) -> &[u8] {
+        unsafe {
+            let args_len = self._args_end as usize - self._args_begin as usize;
+            &mut slice::from_raw_parts(self._args_begin as *const u8, args_len)
+        }
+    }
+}
+
+pub fn replace<T>(space_name: &str, tuple: &T) -> Result<(), Box<dyn Error>>
+    where T: Serialize + ?Sized
+{
+    let space_id = unsafe {
+        box_space_id_by_name(space_name.as_bytes().as_ptr() as *const c_char,
+                             space_name.len() as u32)
+    };
+    assert_ne!(space_id, BOX_ID_NIL);
+
+    println!("space id: {}", space_id);
+
+    let buffer = rmp_serde::to_vec(tuple).unwrap();
+    let buffer_begin = buffer.as_ptr();
+    let buffer_end = unsafe { buffer_begin.offset(buffer.len() as isize) };
+
+
+
+    unsafe {
+        let res = box_replace(space_id, buffer_begin.cast(), buffer_end.cast(), 0 as *mut *mut BoxTuple);
+        assert_eq!(res, 0);
+    };
+
+    Ok(())
+}
+
+pub fn proc_error(file: &str, line: u32, message: &str) {
+    const ER_PROC_C: u32 = 102;
+    unsafe {
+        box_error_set(file.as_ptr() as *const c_char, line, ER_PROC_C, "%s".as_ptr() as *const c_char, message);
+    };
+}
+
